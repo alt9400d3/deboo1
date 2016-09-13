accdb015af26930a6c36007d61edef483b1ea5f7
Oleh Kopylov <alt9400d3@gmail.com>
2016-09-13 20:42:06 +0300

    sdf

--- manage.py
+++ manage.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 import os
-import sys
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+from django.core import management
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+    management.execute_from_command_line()
